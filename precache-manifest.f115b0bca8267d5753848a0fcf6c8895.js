08377039cefeb4c1e9da077b5c46e0142d0e3753
Rolkyssoh <juventussoh@gmail.com>
2020-06-04 18:23:46 +0200

    Updates

---
+++ precache-manifest.f115b0bca8267d5753848a0fcf6c8895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33c887fca9dc2f694b022a9c50adf32",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4bbc1518f26ecb86a31",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/static/css/main.09f84f6d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bbc1518f26ecb86a31",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/static/js/main.b76cc553.chunk.js"
+  },
+  {
+    "revision": "a31528228e5634006c39",
+    "url": "/://Rolkyssoh.github.io/monsters-rolodex/static/js/runtime-main.fdc12516.js"
+  }
+]);
